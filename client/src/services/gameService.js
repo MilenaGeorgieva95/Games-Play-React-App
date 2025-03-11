75862bb8b61e0e59844069e25263a1b6eca12402
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-03-11 23:12:08 +0000

    add game service

---
+++ client/src/services/gameService.js
@@ -0,0 +1,16 @@
+baseUrl = "http://localhost:/jsonstore/games";
+
+export default {
+  async create(gameData) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(gameData),
+    };
+    const response = await fetch(baseUrl, options);
+    const data = await response.json();
+    return data;
+  },
+};
