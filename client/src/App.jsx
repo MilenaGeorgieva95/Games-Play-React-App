ccfd61ba4711775adacf341b38915faa7873a63a
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-03-11 22:53:07 +0000

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Catalog from "./components/Catalog/Catalog";
+import Register from "./components/Authentication/Register";
+import Login from "./components/Authentication/Login";
 
 function App() {
   return (
     <div id="box">
       <main id="main-content">
         <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/auth">
+            <Route path="/auth/register" element={<Register />} />
+            <Route path="/auth/login" element={<Login />} />
+          </Route>
+        </Routes>
       </main>
     </div>
   );
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,23 +1,25 @@
+import { Link } from "react-router";
+
 export default function Header() {
   return (
     <header>
       {/* <!-- Navigation --> */}
       <h1>
-        <a className="home" href="#">
+        <Link className="home" to="/">
           GamesPlay
-        </a>
+        </Link>
       </h1>
       <nav>
-        <a href="#">All games</a>
+        <Link to="/catalog">All games</Link>
         {/* <!-- Logged-in users --> */}
         <div id="user">
-          <a href="#">Create Game</a>
-          <a href="#">Logout</a>
+          <Link to="/create">Create Game</Link>
+          <Link to="/auth/logout">Logout</Link>
         </div>
         {/* <!-- Guest users --> */}
         <div id="guest">
-          <a href="#">Login</a>
-          <a href="#">Register</a>
+          <Link to="/auth/login">Login</Link>
+          <Link to="/auth/register">Register</Link>
         </div>
       </nav>
     </header>
