a53496badb838afc53db37c388c8cb383a9d3c16
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-03-20 22:02:59 +0000

    add username field to register

--- client/src/components/Authentication/Register.jsx
+++ client/src/components/Authentication/Register.jsx
@@ -6,7 +6,15 @@ export default function Register() {
           <div className="brand-logo"></div>
           <h1>Register</h1>
 
-          <label for="email">Email:</label>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="username"
+            id="username"
+            name="username"
+            placeholder="maria"
+          />
+
+          <label htmlFor="email">Email:</label>
           <input
             type="email"
             id="email"
@@ -14,10 +22,10 @@ export default function Register() {
             placeholder="maria@email.com"
           />
 
-          <label for="pass">Password:</label>
+          <label htmlFor="pass">Password:</label>
           <input type="password" name="password" id="register-password" />
 
-          <label for="con-pass">Confirm Password:</label>
+          <label htmlFor="con-pass">Confirm Password:</label>
           <input
             type="password"
             name="confirm-password"
