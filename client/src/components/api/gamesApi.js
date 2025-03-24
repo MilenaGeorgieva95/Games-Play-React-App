1cd0428ff3776bf8cb4da516488e861f5b772b30
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-03-24 09:41:19 +0000

    add authorized create game functionality

---
+++ client/src/components/api/gamesApi.js
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+import { request } from "../utils/requester";
+import { UserContext } from "../contexts/UserContext";
+
+const baseUrl = "/data/games";
+
+export default {
+  create(gameData) {
+    const data = request.post(baseUrl, gameData);
+    return data;
+  },
+  async getAll() {
+    const data = await request.get(baseUrl);
+    const games = Object.values(data);
+    return games;
+  },
+  async getOne(gameId) {
+    const data = await request.get(`${baseUrl}/${gameId}`, "");
+    return data;
+  },
+  del(gameId) {
+    return request.del(`${baseUrl}/${gameId}`, "");
+  },
+  edit(gameId, gameData) {
+    gameData._id = gameId;
+    return request.put(`${baseUrl}/${gameId}`, gameData);
+  },
+};
+
+export const useCreateGame = () => {
+  const { accessToken } = useContext(UserContext);
+  create = (gameData) => {
+    return request.post(baseUrl, gameData, accessToken);
+  };
+  return { create };
+};
