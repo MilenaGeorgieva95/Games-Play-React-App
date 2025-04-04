3c787f172d2012d98a3bd39a47e95066fef00224
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-04-04 10:22:21 +0100

    fetch comments in componentDidMount

--- client/src/components/admin/admin-comments/AdminComments.jsx
+++ client/src/components/admin/admin-comments/AdminComments.jsx
@@ -1,6 +1,13 @@
 import { Component } from "react";
+import { request } from "../../../utils/requester";
+
+const commentsUrl = "/data/comments";
 
 export default class AdminComments extends Component {
+  async componentDidMount() {
+    const comments = await request.get(commentsUrl);
+    console.log(comments);
+  }
   render() {
     return (
       <ul>
